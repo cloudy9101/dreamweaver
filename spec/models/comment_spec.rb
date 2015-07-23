8769138cd5f44bf47f36c0a98ca5682b062ab595
cloudy9101 <cloudy9101@126.com>
2015-07-23 16:08:26 +0800

    test auto deploy

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -1,5 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe Comment, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context 'error' do
+    it 'return error' do
+      expect(false).to eq true
+    end
+  end
 end
