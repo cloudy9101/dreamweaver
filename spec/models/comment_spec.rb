697513f51377bc9d971d3195bb37c99851726d40
cloudy9101 <cloudy9101@126.com>
2015-07-23 16:26:52 +0800

    test ci psss

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -1,9 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe Comment, type: :model do
-  context 'error' do
-    it 'return error' do
-      expect(false).to eq true
-    end
-  end
+#  context 'error' do
+#    it 'return error' do
+#      expect(false).to eq true
+#    end
+#  end
 end
